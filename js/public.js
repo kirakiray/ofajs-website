31dc33ceadf2bf5e022b76740e89dceb57c05d15
YAO <pikay@foxmail.com>
2020-03-11 15:21:15 +0800

    Update public.js

--- js/public.js
+++ js/public.js
@@ -1,7 +1,7 @@
 (() => {
     let script = document.createElement("script");
     script.setAttribute("type", `text/javascript`);
-    script.setAttribute("src", `http://tajs.qq.com/stats?sId=66526840`);
+    script.setAttribute("src", `https://tajs.qq.com/stats?sId=66526840`);
     script.setAttribute("charset", `UTF-8`);
     document.head.appendChild(script);
 })()
