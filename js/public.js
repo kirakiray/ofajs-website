98a3d024dd914bf3c8e3f6eb59d09d621d449c6d
YAO <pikay@foxmail.com>
2020-03-11 15:08:05 +0800

    add tajs

---
+++ js/public.js
@@ -0,0 +1,7 @@
+(() => {
+    let script = document.createElement("script");
+    script.setAttribute("type", `text/javascript`);
+    script.setAttribute("src", `http://tajs.qq.com/stats?sId=66526840`);
+    script.setAttribute("charset", `UTF-8`);
+    document.head.appendChild(script);
+})()
