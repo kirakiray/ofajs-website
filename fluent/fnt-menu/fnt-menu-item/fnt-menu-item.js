89fad3820dac9b09e4d1b44035da918dd4ccf2ac
YAO <kirayao@hotmail.com>
2020-07-19 23:11:38 +0800

    feat: add fnt-menu

---
+++ fluent/fnt-menu/fnt-menu-item/fnt-menu-item.js
@@ -0,0 +1,12 @@
+Component({
+    tag: "fnt-menu-item",
+    temp: true,
+    attrs: {
+        // 是否激活状态
+        active: null
+    },
+    data: {
+        // 是否竖的方向，默认横向
+        vertical: null
+    }
+})
---
+++ fluent/fnt-menu/fnt-menu.js
@@ -0,0 +1,57 @@
+Component(async (load) => {
+    await load("./fnt-menu-item -p");
+
+    return {
+        tag: "fnt-menu",
+        temp: true,
+        css: true,
+        attrs: {
+            // 是否锤子
+            vertical: null,
+            // 颜色
+            color: null,
+            // 是否反色模式
+            inverted: null
+        },
+        watch: {
+            vertical() {
+                $.nextTick(() => {
+                    this.refreshLine();
+                });
+            }
+        },
+        proto: {
+            // 刷新UI的激活线
+            refreshLine() {
+                let activeItem = this.$("fnt-menu-item[active]");
+
+                if (this.vertical === null) {
+                    Object.assign(this.$line.style, {
+                        width: activeItem.width + "px",
+                        height: "",
+                        left: activeItem.ele.offsetLeft + "px",
+                        top: ""
+                    });
+                } else {
+                    Object.assign(this.$line.style, {
+                        width: "",
+                        height: activeItem.height + "px",
+                        left: "",
+                        top: activeItem.ele.offsetTop + "px"
+                    });
+                }
+            }
+        },
+        ready() {
+            this.on("click", "fnt-menu-item", e => {
+                this.all(`fnt-menu-item[active]`).forEach(e => e.attrs.active = null);
+                e.target.attrs.active = "";
+                this.refreshLine();
+            });
+
+            setTimeout(() => {
+                this.refreshLine();
+            }, 100);
+        }
+    };
+});
