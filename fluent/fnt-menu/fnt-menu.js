3c5e196f66d03c43516f73047b7e4ffd845d4fac
YAO <kirayao@hotmail.com>
2020-07-22 21:50:12 +0800

    feat: update fnt-menu

--- fluent/fnt-menu/fnt-menu.js
+++ fluent/fnt-menu/fnt-menu.js
@@ -38,19 +38,42 @@ Component(async (load) => {
             refreshLine() {
                 let activeItem = this.$("fnt-menu-item[active]");
 
+                // 清空默认样式
+                Object.assign(this.$line.style, {
+                    width: "",
+                    left: "",
+                    height: "",
+                    top: ""
+                });
+
+                // 设置非激活状态
+                this._setUnActiveLine(activeItem);
+            },
+            // 设置激活line样式
+            _setActiveLine(activeItem) {
                 if (this.vertical === null) {
                     Object.assign(this.$line.style, {
                         width: activeItem.width + "px",
-                        height: "",
                         left: activeItem.ele.offsetLeft + "px",
-                        top: ""
                     });
                 } else {
                     Object.assign(this.$line.style, {
-                        width: "",
                         height: activeItem.height + "px",
-                        left: "",
                         top: activeItem.ele.offsetTop + "px"
+                    });
+                }
+            },
+            // 设置line为非激活状态
+            _setUnActiveLine(activeItem) {
+                if (this.vertical === null) {
+                    Object.assign(this.$line.style, {
+                        width: activeItem.width - 16 + "px",
+                        left: activeItem.ele.offsetLeft + 8 + "px",
+                    });
+                } else {
+                    Object.assign(this.$line.style, {
+                        height: activeItem.height - 16 + "px",
+                        top: activeItem.ele.offsetTop + 8 + "px",
                     });
                 }
             }
@@ -60,11 +83,32 @@ Component(async (load) => {
                 this.all(`fnt-menu-item[active]`).forEach(e => e.attrs.active = null);
                 e.target.attrs.active = "";
                 this.refreshLine();
+                this._setActiveLine(e.target);
             });
 
+            // 延迟更新ui
             setTimeout(() => {
                 this.refreshLine();
             }, 100);
+
+            // hover更新宽度
+            this.on("mouseover", "fnt-menu-item", e => {
+                // 横向
+                let activeItem = this.$("fnt-menu-item[active]");
+
+                // 判断是否跟激活状态相等，是的话就进行下划线修正
+                if (e.target === activeItem) {
+                    this._setActiveLine(activeItem);
+                }
+            });
+
+            this.on("mouseout", "fnt-menu-item", e => {
+                let activeItem = this.$("fnt-menu-item[active]");
+
+                if (e.target === activeItem) {
+                    this._setUnActiveLine(activeItem);
+                }
+            });
         }
     };
 });
