81ca7bb2fa6d838a1cb2c6e4eeff01cfc4ac62e4
YAO <kirayao@hotmail.com>
2021-01-26 23:38:05 +0800

    feat: 添加开发历史图

--- o-book/bc/articleAside/articleAside.js
+++ o-book/bc/articleAside/articleAside.js
@@ -11,6 +11,9 @@ Component(async (load) => {
             // 设置激活状态
             set titleActive(index) {
                 this.$tabs.activeIndex = index;
+            },
+            get article() {
+                return this.$host.$app.currentPage.$article;
             }
         },
         ready() {
@@ -18,7 +21,7 @@ Component(async (load) => {
             this.$tabs.on("click", 'fnt-tabs-item', e => {
                 let title = e.delegateTarget.title;
 
-                let targetTitle = this.$host.$app.currentPage.$article.all('h1,h2,h3,h4,h5').find(e => e.text == title);
+                let targetTitle = this.article.all('h1,h2,h3,h4,h5').find(e => e.text == title);
                 targetTitle.ele.scrollIntoView({
                     behavior: "smooth", block: "start", inline: "nearest"
                 });
